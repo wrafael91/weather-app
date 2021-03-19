d1cbe0fc1eb7f7699eeffff76b155ef6dbaa0ac4
wrafael91 <wrafaelmp91@gmail.com>
2021-03-19 17:36:58 -0500

    third commit

--- app.js
+++ app.js
@@ -7,13 +7,16 @@
     bogotaHttps.addEventListener('load', (data) => {
         const dataJSON = JSON.parse(data.target.response);
 
-        const list = document.getElementById('bog-list');
-        for (const weatherInfo of dataJSON) {
-            const listItem = document.createElement('LI');
-            listItem.textContent = `${weatherInfo.main} -- ${weatherInfo.weather[1]}`
-            list.appendChild(listItem)
-        }
+        // const list = document.getElementById('bog-list');
+        // const listItem = document.createElement('');
+        // listItem.textContent = `${dataJSON.main.temp} -- ${dataJSON.weather[0].main} ${dataJSON.weather[0].icon}`
+        // list.appendChild(listItem)
+        const { main, icon } = dataJSON.weather[0];
+        const { temp } = dataJSON.main;
+        document.querySelector('.bog-temp').innerText = (temp-273.15) + '°C';
+        document.querySelector('.icon').src = "https://api.openweathermap.org/img/wn/" + icon + "@2x.png";
+        
     })
-
+    
     bogotaHttps.send();
 })()
