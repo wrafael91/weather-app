8ab583f266ed9330d8f44336899a8c2b751b853a
wrafael91 <wrafaelmp91@gmail.com>
2021-03-19 23:59:05 -0500

    fourth commit

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-(() => {
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=Bogota&appid=5be15f27f180f07259d63b580db53929";
-    const bogotaHttps = new XMLHttpRequest();
+//Banner Bogotá
+const bogotaUrl = "http://api.openweathermap.org/data/2.5/weather?q=Bogota&appid=5be15f27f180f07259d63b580db53929";
+const bogotaHttps = new XMLHttpRequest();
 
-    bogotaHttps.open("GET", url);
+bogotaHttps.open("GET", bogotaUrl);
 
-    bogotaHttps.addEventListener('load', (data) => {
-        const dataJSON = JSON.parse(data.target.response);
-
-        // const list = document.getElementById('bog-list');
-        // const listItem = document.createElement('');
-        // listItem.textContent = `${dataJSON.main.temp} -- ${dataJSON.weather[0].main} ${dataJSON.weather[0].icon}`
-        // list.appendChild(listItem)
-        const { main, icon } = dataJSON.weather[0];
-        const { temp } = dataJSON.main;
-        document.querySelector('.bog-temp').innerText = (temp-273.15) + '°C';
-        document.querySelector('.icon').src = "https://api.openweathermap.org/img/wn/" + icon + "@2x.png";
-        
-    })
+bogotaHttps.addEventListener('load', (data) => {
+    const dataJSON = JSON.parse(data.target.response);
+    const { icon, description } = dataJSON.weather[0];
+    const { temp } = dataJSON.main;
+    document.querySelector('.bog-temp').innerText = (temp-273.15) + '°C';
+    document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector('.description').innerText = description;
     
-    bogotaHttps.send();
-})()
+})
+bogotaHttps.send();
+
+//3 Days Forecast Section
+
